e8f4ca97b69bb96b9b9ebd1a0a7efac57320d101
Alan Kang <alankang@fortinet.com>
2018-07-20 16:11:18 -0700

    array

--- src/argtype.rs
+++ src/argtype.rs
@@ -12,64 +12,32 @@ impl<T: Any> AsAny for T {
     fn as_any(&self) -> &Any { self }
 }
 
+macro_rules! get_option_value {
+    ($v:ident, $f:ident) => {
+        if let Some(fs) = $f {
+            if let Some(vs) = $v {
+                vs.clone_from(fs);
+            } else {
+                *$v = Some(fs.clone());
+            }
+        } else {
+            if let Some(_) = $v {
+                *$v = None;
+            }
+        }
+    }
+}
+
 impl ArgType {
     pub fn get_value(&self, to: &mut ArgType) {
         match (to, self) {
-            (Text(v), Text(f))   => {
-                if let Some(fs) = f {
-                    if let Some(vs) = v {
-                        vs.clone_from(fs);
-                    } else {
-                        *v = Some(fs.clone());
-                    }
-                } else {
-                    if let Some(_) = v {
-                        *v = None;
-                    }
-                }
-            },
-            (Texts(v), Texts(f))   => {
-                if let Some(fs) = f {
-                    if let Some(vs) = v {
-                        vs.clone_from(fs);
-                    } else {
-                        *v = Some(fs.clone());
-                    }
-                } else {
-                    if let Some(_) = v {
-                        *v = None;
-                    }
-                }
-            },
-            (Ints(v), Ints(f))   => {
-                if let Some(fs) = f {
-                    if let Some(vs) = v {
-                        vs.clone_from(fs);
-                    } else {
-                        *v = Some(fs.clone());
-                    }
-                } else {
-                    if let Some(_) = v {
-                        *v = None;
-                    }
-                }
-            },
-            (Floats(v), Floats(f))   => {
-                if let Some(fs) = f {
-                    if let Some(vs) = v {
-                        vs.clone_from(fs);
-                    } else {
-                        *v = Some(fs.clone());
-                    }
-                } else {
-                    if let Some(_) = v {
-                        *v = None;
-                    }
-                }
-            },
-            (Int(v), Int(f))   => { *v = *f; },
-            (Float(v), Float(f)) => { *v = *f; },
-            (BoolFlag(v), BoolFlag(f))     => { *v = *f; },
+            (Text(v), Text(f))         => { get_option_value!(v, f); },
+            (Texts(v), Texts(f))       => { get_option_value!(v, f); },
+            (Ints(v), Ints(f))         => { get_option_value!(v, f); },
+            (Floats(v), Floats(f))     => { get_option_value!(v, f); },
+            (Int(v), Int(f))           => { *v = *f; },
+            (Float(v), Float(f))       => { *v = *f; },
+            (BoolFlag(v), BoolFlag(f)) => { *v = *f; },
             (IncFlag(v), IncFlag(f))   => { *v = *f; },
             (Custom(_), _) => {
                 panic!("use get_custom_value() for custom type");
